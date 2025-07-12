8260a196bb9fdda9a9e604463cad6a9b2f7e204b
Ivan A Rodriguez <ivancalvo91@gmail.com>
2025-07-11 22:45:11 -0400

    Adding campground routes - Features:    -Users can add a campground    -Campground can be listed    - Single campground can be viewed

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/help-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'database connection error'));
+db.once('open', () => console.log('Database connected'));
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
+	res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/create', async (req, res) => {
+	res.render('campgrounds/create');
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const { campground } = req.body;
+	if (!campground) {
+		res.status(400);
+		return res.json({ message: 'campground is missing' });
+	}
+
+	const camp = new Campground(campground);
+	await camp.save();
+	return res.redirect(`/campgrounds/${camp._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	console.log(campground);
+	res.render('campgrounds/show', { campground });
 });
 
 app.listen('3000', () => {
---
+++ seed/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelper');
+
+mongoose.connect('mongodb://localhost:27017/help-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'database connection error'));
+db.once('open', () => console.log('Database connected'));
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDatabase = async (quantity) => {
+	console.log('Seeding started...');
+	await Campground.deleteMany({});
+	const promises = [];
+	console.log('All campgrounds have been deleted');
+	for (let i = 0; i < quantity ?? 0; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const campground = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${sample(descriptors)} ${sample(places)}`,
+		});
+		promises.push(campground.save());
+	}
+
+	await Promise.all(promises);
+	console.log('All campgrounds have been created');
+};
+
+seedDatabase(50)
+	.then(() => console.log('Seeding completed'))
+	.catch((e) => console.error(`seeding failed: ${e}`))
+	.finally(async () => {
+		await mongoose.connection.close();
+	});
